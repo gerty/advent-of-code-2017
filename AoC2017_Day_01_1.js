2144909fd341022f23e193a358253da5294d8ddd
gerty <gerty33@gmail.com>
2017-11-30 23:40:58 -0600

    Simplifications and comments

--- AoC2017_Day_01_1.js
+++ AoC2017_Day_01_1.js
@@ -8,25 +8,23 @@
 console.log('Hello to the world...');
 
 var fs = require("fs");
-var dailyinput = '';
+var dailyinput = '';		// init daily input string
 var data = fs.readFileSync('input_day_01.txt'); // CHANGE FILENAME DAILY!!
 //var data = fs.readFileSync('test_input_01.txt'); // CHANGE FILENAME DAILY!!
 dailyinput = data.toString();
 
-var min = 0;
 var len = 0;
-var max = 0;
-var i = 0;
-
-len = dailyinput.length;
-
-console.log('Today\'s input has ' + len + ' characters.');
 var answer = 0;
 
-for (var i = 0; i<len; i++) {
-	if (dailyinput[i] == dailyinput[(i+(len/2)) % len]) { // Part 1 "(len/2)" was "1"
-		answer = answer + parseInt(dailyinput[i]);
-		console.log("Adding " + dailyinput[i]);
+len = dailyinput.length; // How long is the input? A useful thing to know.
+
+console.log('Today\'s input has ' + len + ' characters.');
+
+for (var i = 0; i<len; i++) {  // traverse the string
+//	if (dailyinput[i] == dailyinput[   (i+1)    % len]) { // Part 1
+	if (dailyinput[i] == dailyinput[(i+(len/2)) % len]) { // Part 2
+		answer = answer + parseInt(dailyinput[i]);  // need to add the char as an int
+		console.log("Adding " + dailyinput[i]);  // just reporting to the console
 	}
 }
 
