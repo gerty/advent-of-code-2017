3c1d4047c2701821eaf3cb65909c79361588a2a1
gerty <gerty33@gmail.com>
2017-11-30 21:33:19 -0600

    Template for day 1 complete

--- AoC2017_Day_01_1.js
+++ AoC2017_Day_01_1.js
@@ -1,1 +1,25 @@
+// Advent Of Code Puzzle #1 - (name of puzzle here)
+// Programming in JavaScript via command line node
+// Repo located at https://github.com/gerty/advent-of-code-2017
+// Comments and suggestions welcome
+
+// 12/1/17 - Happy Holidays to one and all! I'm in a good mood and ready to start!
+
 console.log('Hello to the world...');
+const day_1_input = 'DAY1INPUTHERE';
+
+var min = 0;
+var len = 0;
+var max = 0;
+var i = 0;
+
+len = day_1_input.length;
+data = day_1_input;
+
+console.log('Day 1 input has ' + len + ' characters.');
+
+for (var i = 0; i<len; i++) {
+	console.log(data[i]);
+}
+
+console.log('Answer to Day 1 = ' + 'NOT YET');
