3aa3d4927dd4674831ab85d256708559a3dd343a
gerty <gerty33@gmail.com>
2017-12-03 01:35:47 -0600

    Day 3 messy but complete

--- Day 03 - /AoC2017_Day_03.js
+++ Day 03 - /AoC2017_Day_03.js
@@ -7,42 +7,88 @@
 
 console.log('Hello to the world...day 3 is come!');
 
-var fs = require("fs");
-var dailyinput = '';		// init daily input string
-var data = fs.readFileSync('input_day_03.txt'); // CHANGE FILENAME DAILY!!
+//var fs = require("fs");
+//var dailyinput = '';		// init daily input string
+//var data = fs.readFileSync('input_day_03.txt'); // CHANGE FILENAME DAILY!!
 //var data = fs.readFileSync('test_input_03.txt'); // CHANGE FILENAME DAILY!!
-dailyinput = data.toString();
+//dailyinput = data.toString();
 
-var len = 0;
-var inputArray = new Array(); // usually an array is involved. Leave length arbitrary.
-var answer1 = 0;
-var answer2 = 0;
+const puzzleInput = 368078;
+const arrayWidth = 20;
+console.log('Puzzle Input is: ' + puzzleInput + ' and width of array is: ' + arrayWidth);
+var inputArray = new Array(arrayWidth); // usually array is involved. length arbitrary.
 
-for (var z=0; z<16; z++) {  // for 2-dimensional array of 16
-	inputArray[z] = new Array(); 
-}
-
-len = dailyinput.length; // How long is the input? A useful thing to know.
-console.log(dailyinput);
-console.log('Today\'s input has ' + len + ' characters.');
-
-var lines = dailyinput.split('\n'); // splitting the input into lines by newline
-for (var i=0;i<lines.length;i++) {
-	inputArray[i] = lines[i].split('\t'); // splitting the lines into items by tabs
-}
-
-console.log(inputArray);
-
-for (var i=0;i<16;i++) {
-	for (var j=0;j<16;j++) { // now we go line by line through a 16x16 array
-		// do something important
+for (var i=0; i<arrayWidth; i++) {  // for 2-dimensional array of arrayWidth
+	inputArray[i] = new Array(arrayWidth);
+	for (var j=0; j<arrayWidth; j++) {
+		inputArray[i][j] = 0;
 	}
 }
+var answer2 = 0;
 
-for (var sum=0;sum<16;sum++){
-	answer1 += divisibleCollection[sum];
+function surrounding(inArray,x,y) {
+	var calc = (inArray[x-1][y-1] + 	// NW
+		   		inArray[x][y-1] +	// N
+			    inArray[x+1][y-1] +	// NE
+			    inArray[x+1][y] +	// E
+			    inArray[x+1][y+1] +	// SE
+			    inArray[x][y+1] +	// S
+			    inArray[x-1][y+1] +	// SW
+			    inArray[x-1][y]);	// W
+	if ((answer2 == 0) && (calc > puzzleInput)) {		// check for winner
+		console.log('Answer is : ' + answer2); 
+		answer2 = calc;
+	}
+	return(calc);
 }
 
-console.log('Answer to Day 3 Part 1 = ' + answer1);
+var xPos = Math.floor(arrayWidth/2);
+var yPos = Math.floor(arrayWidth/2);
+
+console.log('Today\'s input is ' + puzzleInput + '.');
+
+inputArray[xPos][yPos] = 1; // Start out with 1 in the center
+
+
+for (var square=3;square<13;square=square+2) // spiral CCW to RT
+	{
+		xPos++; // start off the ring to the right
+		inputArray[xPos][yPos] = surrounding(inputArray,xPos,yPos); // load first position
+		console.log(xPos,yPos,inputArray[xPos][yPos]);
+		
+		for (var delta=0;delta<square-2;delta++) // up  right side (minus the one we did)
+		{
+			yPos--;
+			inputArray[xPos][yPos] = surrounding(inputArray,xPos,yPos); // load cell
+			console.log(inputArray[xPos][yPos]);
+		}
+
+		for (var delta=0;delta<square-1;delta++) // across the top to the left
+		{
+			xPos--;
+			inputArray[xPos][yPos] = surrounding(inputArray,xPos,yPos); // load cell
+			console.log(inputArray[xPos][yPos]);
+		}
+		
+		for (var delta=0;delta<square-1;delta++) // down the left side
+		{
+			yPos++;
+			inputArray[xPos][yPos] = surrounding(inputArray,xPos,yPos); // load cell
+			console.log(inputArray[xPos][yPos]);
+		}
+		
+		for (var delta=0;delta<square-1;delta++) // across the bottom to the right
+		{
+			xPos++;
+			inputArray[xPos][yPos] = surrounding(inputArray,xPos,yPos); // load cell
+			console.log(inputArray[xPos][yPos]);
+		}
+	}
+
+console.log('Ending at x,y: ' + xPos + ',' + yPos);
+
+console.log('Answer to Day 3 Part 1 = 371 (figured it out manually)');
 
 console.log('Answer to Day 3 Part 2 = ' + answer2);
+
+console.log(inputArray);
