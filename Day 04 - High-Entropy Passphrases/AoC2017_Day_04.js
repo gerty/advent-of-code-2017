ba87d662a968b9b57e48c26426383807f038533b
gerty <gerty33@gmail.com>
2017-12-05 01:19:39 -0600

    Whew - needs a cleanup. But done with Day 4 and 5 now.

--- Day 04 - High-Entropy Passphrases/AoC2017_Day_04.js
+++ Day 04 - High-Entropy Passphrases/AoC2017_Day_04.js
@@ -17,27 +17,35 @@ var len = 0;
 var inputArray = new Array(); // usually an array is involved. Leave length arbitrary.
 var answer1 = 0;
 var answer2 = 0;
+var anagram = false;
 
 len = dailyinput.length; // How long is the input? A useful thing to know.
 console.log('Today\'s input has ' + len + ' characters.');
 
 var lines = dailyinput.split('\n'); // splitting the input into lines by newline
+
 for (var i=0;i<lines.length;i++) {
 	inputArray = lines[i].split(" "); // splitting the lines into items by spaces
 	// Lets find the number of bad passphrases in this line
-	for (var j=1;j<inputArray.length;j++) { // now we go line by line through array
+	for (var j=1;j<inputArray.length;j++) { // now we go word by word through line
 		for (var k=0;k<j;k++) { // only go back and check behind you in array
-			if (inputArray[j].toLowerCase().split('').sort().join('').trim() === inputArray[k].toLowerCase().split('').sort().join('').trim()) {
-				answer1++;
+			// console.log('j=' + j + ' k=' + k);
+			if (inputArray[j].split('').sort().join('').trim() === inputArray[k].split('').sort().join('').trim()) {
+				anagram = true;
 				console.log("Whoops: " + inputArray[j] + " is an anagram of " + inputArray[k]);
-				j = inputArray.length; // zoom to end of array so you don't count twice
 			}
 		}
+	}
+	if (anagram == true) {
+		answer2++;
+		anagram = false;
+		console.log('Line ' + i + ' has ' + answer2);
 	}
 }
 console.log('Today\'s input has ' + lines.length + ' lines.');
 
 answer1 = lines.length - answer1; // counted the invalid - subtract out the valid
+answer2 = lines.length - answer2; // counted the invalid - subtract out the valid
 console.log('Answer to Day 4 Part 1 = ' + answer1); // not 22521 // not 513 // not 467
 
-console.log('Answer to Day 4 Part 2 = ' + answer2);
+console.log('Answer to Day 4 Part 2 = ' + answer2); // not 195 (low) not 403 (high) 261 (high)
