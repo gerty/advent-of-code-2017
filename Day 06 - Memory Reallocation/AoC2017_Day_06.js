fb61c1e308ef7ff2cd77f1ced268c07a7ac452bc
gerty <gerty33@gmail.com>
2017-12-06 00:13:25 -0600

    Possibly calling it a night.

--- Day 06 - Memory Reallocation/AoC2017_Day_06.js
+++ Day 06 - Memory Reallocation/AoC2017_Day_06.js
@@ -7,48 +7,58 @@
 console.log('Hello to the world...day 6 is come!');
 
 var fs = require("fs");
+var crypto = require("crypto");
 var dailyinput = '';		// init daily input string
-//var data = fs.readFileSync('input.txt');
-var data = fs.readFileSync('test.txt');
+var data = fs.readFileSync('input.txt');
+//var data = fs.readFileSync('test.txt');
 dailyinput = data.toString();
 
-var inputLength = 0;
-var count = 0;
 var numArray = new Array();
-var wordArray = new Array();
-var intArray = new Array();
+var answerArray = new Array();
+var count = 0;
 var answer1 = 0;
 var answer2 = 0;
 
-inputLength = dailyinput.length; // How long is the input? A useful thing to know.
+var inputLength = dailyinput.length; // How long is the input? A useful thing to know.
 console.log('Today\'s input has ' + inputLength + ' characters.');
 
-var lines = dailyinput.split('\n'); // splitting the input into lines by newline
+var lines = dailyinput.split('\t'); // splitting the input into strings by tabs
 // now we have an array of strings for each line
 
-for (var z=0;z<lines.length;z++) {    // if each line contains a single integer
+console.log('Today\'s input has ' + lines.length + ' items.');
+
+for (var z=0;z<lines.length;z++) {    // if each string contains a single integer
 	numArray[z] = parseInt(lines[z]); // convert to an array of integers
 }
 
-for (var z=0;z<lines.length;z++) {      // if each line contains multiple entries
-	wordArray[z] = lines[z].split(' '); // split elements on each line by spaces
-}
 console.log(lines);
-for (var z=0;z<lines.length;z++) {
-	for (y=0;y<lines[z].length;y++) {
-		console.log(z);
-		console.log(y);
-		intArray[z] = new Array();
-		intArray[z][y] = parseInt(lines[z][y]); // on each line convert to ints, put in array
+console.log(numArray); // work with numArray today
+
+
+while (answer1 == 0) {
+	if (ansChecksum in answerArray) { // if the current array is in the history
+		answer1 = count; // the answer would be the count
 	}
-}
+	answerArray[count++] = ansChecksum; // add to history
+	
+	console.log(ansChecksum);
 
-//console.log(lines);
-//console.log(numArray);
-//console.log(wordArray);
-//console.log(intArray);
-
-console.log('Today\'s input has ' + lines.length + ' lines.');
+// two ideas: one to put each answer in a hash table, then look up to see if anything is there
+// 			  another idea is to use a linked list to store the value and pointer to the next
+	
+	// redistribute
+	var maximum = Math.max(...numArray);
+	var a = numArray.indexOf(maximum);
+	numArray[a] = 0;
+	while (maximum > 0) {
+		a++;
+		if (a > 16) a = 0;
+		numArray[a]++;
+		maximum--;
+	}
+	// done redistributing when original maximum=0 and all are increased by 1
+	// console.log(numArray);
+} // do it again unless you found a match in history
 
 console.log('Answer to Day 6 Part 1 = ' + answer1);
 
