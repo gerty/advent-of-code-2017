8d0237bebf047c7864b294b93589292f52a73bc7
gerty <gerty33@gmail.com>
2017-12-08 00:03:39 -0600

    not quite there yet, but worth sharing

--- Day 08 - I Heard You Like Registers/AoC2017_Day_08.js
+++ Day 08 - I Heard You Like Registers/AoC2017_Day_08.js
@@ -12,7 +12,8 @@ var data = fs.readFileSync('input.txt');
 //var data = fs.readFileSync('test.txt');
 dailyinput = data.toString();
 
-var numArray = new Array();
+var myRegisters = {};
+var operatorObjects = {};
 var answer1 = 0;
 var answer2 = 0;
 
@@ -21,11 +22,30 @@ console.log('Today\'s input has ' + dailyinput.length + ' characters.');
 var lines = dailyinput.split('\n'); // splitting the input into lines by newline
 console.log('Today\'s input has ' + lines.length + ' lines.');
 
-for (var z=0;z<lines.length;z++) {  // convert to an array of integers
-	numArray[z] = parseInt(lines[z]);
+for (var z=0;z<lines.length;z++) {  // split lines to find each substring
+	var splitlines = lines[z].split(' ');
+	lines[z] = splitlines;
 }
 
-// console.log(numArray);
+console.log(lines);
+// example: obj["key3"] = "value3";
+
+for (var z=0;z<lines.length;z++) {  // split lines to find each substring
+	var name1 = lines[z][0];
+	var name2 = lines[z][4];
+	if (!(name1 in myRegisters)) myRegisters[name1] = 0;
+	if (!(name2 in myRegisters)) myRegisters[name2] = 0;
+	var proceedString = myRegisters[name2] + ' ' + lines[z][5] + ' ' + lines[z][6];
+	console.log('n1=' + name1 + ' n2=' + name2 + ' ' + proceedString + '=' + eval(proceedString));
+	if ((lines[z][1] === 'inc') && (eval(proceedString))) {
+		myRegisters[name1] += lines[z][2];
+	}
+	if ((lines[z][1] === 'dec') && (eval(proceedString))) {
+		myRegisters[name1] -= lines[z][2];
+	}
+}
+
+console.log(myRegisters);
 
 console.log('Answer to Day 8 Part 1 = ' + answer1); 
 
