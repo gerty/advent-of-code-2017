d3c033542783c0d0d71990dd30f9be14c761b3d7
gerty <gerty33@gmail.com>
2017-12-10 12:47:21 -0600

    First attempts at Day 10

--- Day 10 - Knot Hash/AoC2017_Day_10.js
+++ Day 10 - Knot Hash/AoC2017_Day_10.js
@@ -3,53 +3,65 @@
 // Repo located at https://github.com/gerty/advent-of-code-2017
 // Comments and suggestions welcome
 
-// 12/4-5/17 - Happy Holidays! Work is taking its toll. Quick shot before bed for points.
+// Happy Holidays! This is a breakfast session. Did I remember to water the tree?
 console.log('Hello to the world...day 10 is come!');
 
 var fs = require("fs");
-var dailyinput = '';		// init daily input string
 var data = fs.readFileSync('input.txt');
 //var data = fs.readFileSync('test.txt');
-dailyinput = data.toString();
+const masterLoopSize = 256;
+var dailyInput = data.toString();
 
-var len = 0;
-var count = 0;
-var numArray = new Array();
 var answer1 = 0;
 var answer2 = 0;
+var knotCircle = new Array();
+for (var i=0; i<masterLoopSize; i++) { knotCircle[i] = i; } // load array
 
-len = dailyinput.length; // How long is the input? A useful thing to know.
-console.log('Today\'s input has ' + len + ' characters.');
+// How long is the input? A useful thing to know.
+console.log('Today\'s input has ' + dailyInput.length + ' characters.');
 
-var lines = dailyinput.split('\n'); // splitting the input into lines by newline
+var loopLengths = dailyInput.split(','); // splitting the input into lines by comma
+// then convert to an array of integers
+for (var z=0;z<loopLengths.length;z++) { loopLengths[z] = parseInt(loopLengths[z]); }
+console.log('...and ' + loopLengths.length + ' integers to work with.')
+console.log(loopLengths);
 
-for (var z=0;z<lines.length;z++) {  // convert to an array of integers
-	numArray[z] = parseInt(lines[z]);
+function getArraySection(array,len,curPos) {
+	var output = []; // we return the subsection of the array we're targeting
+	for (var i=0; i<len; i++) {
+		output[i] = array[(curPos+i) % array.length];
+	}
+	console.log('Found: ' + output);
+	console.log('   in: ' + array);	
+	return(output);
 }
 
-// console.log(numArray);
-
-var i = 0;   // true index
-var whereAmI = 0;  // record where you came from each time
-var steps = 0;   // count all the steps (really jumps)
-
-while ((i>=0) && (i<numArray.length)) { // while within the bounds
-	whereAmI = i;  // record starting off point
-	// console.log(numArray[whereAmI] + ' is at index ' + whereAmI);
-	i += numArray[i];  // go to new spot 
-	if (numArray[whereAmI] >= 3) {
-		numArray[whereAmI]--; // increase the spot where you were by 1
+function putArraySection(array,subSection,curPos) {
+	var output = []; // in this case returned array is entire array, reconstructed
+	for (var i=0; i<array.length; i++) {
+		output[i] = array[i];
 	}
-	else {
-		numArray[whereAmI]++; // increase the spot where you were by 1
+	for (var i=0; i<subSection.length; i++) { // place subsection on original content
+		output[curPos + (i % array.length)] = subSection[i];
 	}
-	if ((i>=0) && (i<numArray.length)) {
-		answer2++;
-	}
-} 
-//console.log(numArray);
+	console.log('Before : ' + array);
+	console.log('After  : ' + output);
+	return(output);
+}
 
-console.log('Today\'s input has ' + lines.length + ' lines.');
+var currentPosition = 0;   // true index of where the loops start
+var skipSize = 0;
+
+for (var i=0; i<loopLengths.length; i++) {
+	currentPosition = (currentPosition + skipSize) % masterLoopSize;
+	var sectionToReplace = getArraySection(knotCircle,loopLengths[i],currentPosition);
+	sectionToReplace = sectionToReplace.reverse(); // ?? not sure what function reverses??
+	knotCircle = putArraySection(knotCircle,sectionToReplace,currentPosition);
+	currentPosition = (currentPosition + loopLengths[i]) % masterLoopSize;
+}
+
+console.log(knotCircle);
+answer1 = knotCircle[0] * knotCircle[1]; // 52212 too high
 
 console.log('Answer to Day 10 Part 1 = ' + answer1);
 console.log('Answer to Day 10 Part 2 = ' + answer2);
