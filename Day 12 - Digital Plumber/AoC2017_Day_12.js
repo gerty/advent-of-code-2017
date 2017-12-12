5c72d6026710b0a873501a818f166feb1b1f3fdb
gerty <gerty33@gmail.com>
2017-12-11 23:35:40 -0600

    Part 1 complete

--- Day 12 - Digital Plumber/AoC2017_Day_12.js
+++ Day 12 - Digital Plumber/AoC2017_Day_12.js
@@ -10,17 +10,40 @@ var fs = require("fs");
 var data = fs.readFileSync('input.txt');
 //var data = fs.readFileSync('test.txt');
 var dailyInput = data.toString();
+var parsedInput = [];
 var answer1 = 0;
 var answer2 = 0;
+var allthezerotalkers = new Set();
 
 // How long is the input? A useful thing to know.
 console.log('Today\'s input has ' + dailyInput.length + ' characters.');
 
-var inputInts = dailyInput.split(','); // splitting the input into lines by comma
-// then convert to an array of integers
-for (var z=0;z<inputInts.length;z++) { inputInts[z] = parseInt(inputInts[z]); }
+var eachLine = [];
+var rawInput = dailyInput.split('\n'); // splitting the input into lines by newline
+for (var z=0;z<rawInput.length;z++) { 
+	eachLine[z] = rawInput[z].split('<->');
+	eachLine[z] = eachLine[z][1].trim().split(',');
+	parsedInput[z] = [];
+	parsedInput[z][0] = parseInt(eachLine[0]); 
+}
 
-console.log('Answer to Day 12 Part 1 = ' + answer1);
+function loadZeroSet(target) {
+	for (var i=0; i<eachLine[target].length; i++) {
+		console.log(eachLine[target][i]);
+		if (!allthezerotalkers.has(parseInt(eachLine[target][i]))) {
+			allthezerotalkers.add(parseInt(eachLine[target][i]));
+			loadZeroSet(parseInt(eachLine[target][i]));
+		}
+	}
+}
+
+console.log(eachLine);
+
+loadZeroSet(0);
+
+answer1 = allthezerotalkers.size;
+
+console.log('Answer to Day 12 Part 1 = ' + answer1); // Got it!
 
 // Now for Part 2
 
