3d8aca3d385b3e0eb30468325205eedc12ed9328
gerty <gerty33@gmail.com>
2017-12-12 22:58:07 -0600

    Prepping for Day 13

--- Day 09 - Stream Processing/AoC2017_Day_09.js
+++ Day 09 - Stream Processing/AoC2017_Day_09.js
@@ -29,15 +29,15 @@ var insideGarbage = false;
 var garbageTally = 0;
 
 for (var i=0; i<dailyInput.length; i++) {
-	if (dailyInput[i] === '<') insideGarbage = true;
-	if (insideGarbage && (dailyInput[i] === '!')) {
+	if (!insideGarbage && (dailyInput[i] === '<')) insideGarbage = true;
+	else if (insideGarbage && (dailyInput[i] === '!')) {
 		i=i+1; // ignore '!' and char that follows - noticed input has no '!' near EOF
 	}	
 	else if (dailyInput[i] === '>') insideGarbage = false;
 	else if (!insideGarbage) {
 		firstPass += (dailyInput[i]);
-		garbageTally++;
 	}
+	else if (insideGarbage) { garbageTally++; }
 } 
 
 console.log('AFTER FIRST PASS = ' + firstPass);
@@ -55,4 +55,4 @@ for (var i=0; i<firstPass.length; i++) {
 answer1 = score;
 
 console.log('Answer to Day 9 Part 1 is:' + answer1);
-console.log('Answer to Day 9 Part 2 = ' + garbageTally); // 2620 too low
+console.log('Answer to Day 9 Part 2 = ' + garbageTally); // 2620 too low, 3084 too low
