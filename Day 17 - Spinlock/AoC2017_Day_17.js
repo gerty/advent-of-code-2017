7457a466edde2b39cf02eedef2025b7b0b0ab4cf
gerty <gerty33@gmail.com>
2017-12-17 02:41:14 -0600

    Just a start

--- Day 17 - Spinlock/AoC2017_Day_17.js
+++ Day 17 - Spinlock/AoC2017_Day_17.js
@@ -6,18 +6,20 @@
 // Happy Holidays! Oil change taking too long. No problem... catching up on Day 16.
 console.log('Hello to the world...day 17 is come!');
 
-const inputSpin = 344;
+const inputSpin = 3; // 344 real, 3 test
 var answer1 = 0;
 var answer2 = 0;
-var spinlock = [];
+var spinlock = [0];
+var pos = 0;
 
-for (var z=0;z<2018;z++) {
-	
-	toProcess = inputInts[z].split('/'); 
-	console.log(toProcess);
+for (var z=0;z<8;z++) {
+	pos += inputSpin;
+	pos %= spinlock.length;
+	spinlock = spinlock.splice(pos,0,z); 
+	console.log(spinlock);
 }
 
-
+answer = spinlock[(pos+1) % spinlock.length];
 
 console.log('Answer to Day 16 Part 1 = ' + answer1);
 // Now for Part 2
