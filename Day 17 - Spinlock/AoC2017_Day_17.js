2a41916abc9fb81168e5a04965c1015acbcea4a2
gerty <gerty33@gmail.com>
2017-12-17 22:58:10 -0600

    Setting up for Day 18

--- Day 17 - Spinlock/AoC2017_Day_17.js
+++ Day 17 - Spinlock/AoC2017_Day_17.js
@@ -13,38 +13,49 @@ var spinlock = [0];
 var pos = 0;
 var zeroLocations = new Set();
 
-var startTime = getTime();
+var startTime = Date.now();
 
-for (var z=1;z<=2017;z++) {
-	pos = (pos + inputSpin) % spinlock.length;
-	spinlock.splice(++pos,0,z); 
-	zeroLocations.add(spinlock[(spinlock.indexOf(0)+1)%(spinlock.length)]);	
+//for (var z=1;z<=2017;z++) {
+//	pos = (pos + inputSpin) % spinlock.length;
+//	spinlock.splice(++pos,0,z); 
+//	zeroLocations.add(spinlock[(spinlock.indexOf(0)+1)%(spinlock.length)]);	
+//}
+//answer1 = spinlock[(pos+1)%(spinlock.length)];
+
+//for (var z=1;z<=500000;z++) {
+//	pos = (pos + inputSpin) % spinlock.length;
+//	spinlock.splice(++pos,0,z); 
+//	zeroLocations.add(spinlock[(spinlock.indexOf(0)+1)%(spinlock.length)]);	
+//}
+
+var midTime = Date.now();
+
+function getAllLess(x) {
+	if (x===0) { return(0); }
+	else {
+		return((getAllLess(x-1)+(inputSpin)) % x);
+	}
 }
 
-answer1 = spinlock[(pos+1)%(spinlock.length)];
-
-for (var z=1;z<=5000;z++) {
-	pos = (pos + inputSpin) % spinlock.length;
-	spinlock.splice(++pos,0,z); 
-	zeroLocations.add(spinlock[(spinlock.indexOf(0)+1)%(spinlock.length)]);	
-}
-
-var midTime = getTime();
-
-for (var y=1;y<=5000;y++) {
+for (var y=1;y<=20;y++) {
 	pos = (pos + inputSpin) % spinlock.length;
 	spinlock.splice(++pos,0,y); 
+	console.log(spinlock);
 }
 
-var endTime = getTime();
-var duration1 = (midTime-startTime)/1000;
-var duration2 = (endTime-midTime)/1000;
-answer1 = spinlock[(spinlock.indexOf(0)+1)%(spinlock.length)];
-
-console.log('Runtime without set recording: ' + duration1);
-console.log('Runtime with set recording: ' + duration1);
+for (var i=0; i<20; i++) {
+	console.log('getAllLess(' + i + ') = ' + getAllLess(i));	
+}
 
 
+var endTime = Date.now();
+
+answer2 = spinlock[(spinlock.indexOf(0)+1)%(spinlock.length)];
 console.log('Answer to Day 16 Part 1 = ' + answer1);
+
 // Now for Part 2
+var duration1 = (midTime-startTime)/1000.0;
+var duration2 = (endTime-midTime)/1000.0;
+console.log('Runtime without set recording: ' + duration1);
+console.log('Runtime with set recording: ' + duration2);
 console.log('Answer to Day 16 Part 2 = ' + answer2); // 187773 is too low
---
+++ Day 18 - tbd/AoC2017_Day_18.js
@@ -0,0 +1,34 @@
+// Advent Of Code Puzzle #18 - 
+// Programming in JavaScript via command line node
+// Repo located at https://github.com/gerty/advent-of-code-2017
+// Comments and suggestions welcome
+
+// Happy Holidays! Gearing up to be competitive at least for one star tonight!
+console.log('Hello to the world...day 18 is come!');
+
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+//var data = fs.readFileSync('test.txt');
+var dailyInput = data.toString();
+var answer1 = 0;
+var answer2 = 0;
+
+// How long is the input? A useful thing to know.
+console.log('Today\'s input has ' + dailyInput.length + ' characters.');
+
+var inputInts = dailyInput.split('\n'); // splitting the input into lines by newline
+// then convert to an array of integers
+for (var z=0; z<inputInts.length; z++) {
+	inputInts[z] = inputInts[z].split(':'); 
+}
+
+for (var i=0; i<inputInts.length; i++) { // create firewall, populate rows in input
+// do something with the data?
+}
+
+
+answer1 = 0;
+
+console.log('Answer to Day 18 Part 1 = ' + answer1);
+// Now for Part 2
+console.log('Answer to Day 18 Part 2 = ' + answer2);
