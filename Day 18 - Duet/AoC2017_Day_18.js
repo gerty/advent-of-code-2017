237dfe36fc6c895d48118310661c12a504ba0e2e
gerty <gerty33@gmail.com>
2017-12-18 02:28:43 -0600

    Part 1 complete. Scared of part 2..

--- Day 18 - Duet/AoC2017_Day_18.js
+++ Day 18 - Duet/AoC2017_Day_18.js
@@ -7,8 +7,8 @@
 console.log('Hello to the world...day 18 is come!');
 
 var fs = require("fs");
-//var data = fs.readFileSync('input.txt');
-var data = fs.readFileSync('test.txt');
+var data = fs.readFileSync('input.txt');
+//var data = fs.readFileSync('test.txt');
 var dailyInput = data.toString();
 var answer1 = 0;
 var answer2 = 0;
@@ -23,89 +23,81 @@ for (var z=0; z<allInput.length; z++) {
 }
 
 var frequency = 0;
-var dataNames = [];
-var dataVals = [];
+var myData = {};
 
 console.log(allInput);
 var i = 0;
 
 function getVal(str) { // find the variable value, or converts string to actual number
-	if (parseInt(str) === NaN) return(dataVals[dataNames.indexOf(str)]);
-	else return parseInt(str);
+	if (isNaN(parseInt(str))) {
+		return(myData[str]);
+	}
+	else {
+		return parseInt(str);
+	}
 }
 
-while (i<allInput.length) { 
-	//console.log('testing 123: ' + getVal('123'));
-	//console.log('testing a: ' + getVal('a'));
-	//console.log('testing 0: ' + getVal('0'));
-
-	console.log('Current position in file: ' + i);
+while ((i<allInput.length) && (i>=0)) { 
+	console.log(myData);
+	console.log('Current position in instructions: ' + i);
 	switch(allInput[i][0]) {
 		case 'snd' :
 			console.log('Playing sound with frequency: ' + getVal(allInput[i][1]));
-			soundLastPlayed = getVal(allInput[i][2]);
+			soundLastPlayed = getVal(allInput[i][1]);
 			i++;
 			break;
 		case 'set' :
-			var here = dataNames.indexOf(allInput[i][1]);
-			if (here === -1) {
-				dataNames.push(allInput[i][1]);
-				dataVals.push(getVal(allInput[i][2]));
-				console.log('Setting ' + allInput[i][1] + ' to ' + getVal(allInput[i][2]));
+			if (myData[allInput[i][1]] != undefined) {
+				myData[allInput[i][1]] = getVal(allInput[i][2]);
+				console.log('Adjusting old ' + allInput[i][1] + ' to ' + getVal(allInput[i][2]));
 			}
 			else {
-				dataVals[here] = getVal(allInput[i][2]);
-				console.log('Updating ' + allInput[i][1] + ' to ' + getVal(allInput[i][2]));
+				myData[allInput[i][1]] = getVal(allInput[i][2]);
+				console.log('Setting new ' + allInput[i][1] + ' to ' + getVal(allInput[i][2]));
 			}
 			i++;
 			break;
 		case 'add' :
-			var here = dataNames.indexOf(allInput[i][1]);
-			dataVals[here] += getVal(allInput[i][2]);
-			console.log('Adding ' + getVal(allInput[i][2] + ' to ' + allInput[i][2]);
+			if (myData[allInput[i][1]] === undefined) myData[allInput[i][1]] = 0;
+			myData[allInput[i][1]] += getVal(allInput[i][2]);
+			console.log('Adding ' + getVal(allInput[i][2] + ' to ' + allInput[i][1]));
 			i++;
 			break;
 		case 'mul' :
-			var here = dataNames[getVal(allInput[i][1])];
-			dataVals[here] *= getVal(allInput[i][2]);
-			console.log('Multiplying ' + allInput[i][2] + ' by ' + allInput[i][2]);
+			if (myData[allInput[i][1]] === undefined) myData[allInput[i][1]] = 0;
+			myData[allInput[i][1]] *= getVal(allInput[i][2]);
+			console.log('Multiplying ' + getVal(allInput[i][2] + ' by ' + allInput[i][1]));
 			i++;
 			break;
 		case 'mod' :
-			var here = dataNames[getVal(allInput[i][1])];
-			dataVals[here] %= getVal(allInput[i][2]);
+			if (myData[allInput[i][1]] === undefined) myData[allInput[i][1]] = 0;
+			myData[allInput[i][1]] %= getVal(allInput[i][2]);
+			console.log(allInput[i][1] + ' modulo ' + getVal(allInput[i][2]));
 			i++;
 			break;
 		case 'rcv' :
-			var here = dataNames[getVal(allInput[i][1])];
-			if (dataVals[here] != 0) {
-				dataVals[here] = soundLastPlayed; // is this considered "recovering"?
-				console.log('Just recovered non-zero value of ' + soundLastPlayed);
+			if (getVal(allInput[i][1]) != 0) {
+				myData[allInput[i][1]] = soundLastPlayed; // "recovering"?
+				console.log('Just recovered sound to ' + soundLastPlayed);
+				answer1 = soundLastPlayed;
+				i=-1;
 			}
 			i++;
 			break;
 		case 'jgz' : 
-			var here = dataNames[getVal(allInput[i][1])];
-			console.log('Jump to ' + here);
-			if (dataVals[here] != 0) {
-				if (typeof allInput[i][2] === 'number') {
-					i = i + allInput[i][2];
-				}
-				else {
-					i = i + dataVals[dataNames.indexOf(allInput[i][2])];
-				}
+			if (myData[allInput[i][1]] === 0) {
+				i++;
 			}
-			i++;
+			else {
+				console.log('Jump ahead ' + getVal(allInput[i][1]));
+				i = i + getVal(allInput[i][2]);
+			}
 			break;
 		default :
 			i++;
 			break;
-		console.log('We are in ' + i + ' position in our input.');
 	}
 }
-
-
-answer1 = 0;
 
 console.log('Answer to Day 18 Part 1 = ' + answer1);
 // Now for Part 2
