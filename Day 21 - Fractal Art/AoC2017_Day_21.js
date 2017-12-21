12888523f12cd7c72e3ea75f819e7dfaceb135c5
gerty <gerty33@gmail.com>
2017-12-20 23:07:17 -0600

    Day 21 setup

--- Day 21 - Fractal Art/AoC2017_Day_21.js
+++ Day 21 - Fractal Art/AoC2017_Day_21.js
@@ -1,14 +1,14 @@
-// Advent Of Code Puzzle #18 - Duet
+// Advent Of Code Puzzle #21 - Fractal Art
 // Programming in JavaScript via command line node
 // Repo located at https://github.com/gerty/advent-of-code-2017
 // Comments and suggestions welcome
 
-// Happy Holidays! Gearing up to be competitive at least for one star tonight!
-console.log('Hello to the world...day 18 is come!');
+// Happy Holidays! Board games before 11pm, and Game of Life after?
+console.log('Hello to the world...day 21 is come!');
 
 var fs = require("fs");
-//var data = fs.readFileSync('input.txt');
-var data = fs.readFileSync('test.txt');
+var data = fs.readFileSync('input.txt');
+//var data = fs.readFileSync('test.txt');
 var dailyInput = data.toString();
 var answer1 = 0;
 var answer2 = 0;
@@ -17,18 +17,14 @@ var answer2 = 0;
 console.log('Today\'s input has ' + dailyInput.length + ' characters.');
 
 var inputParsed = dailyInput.split('\n'); // splitting the input into lines by newline
-// then convert to an array of integers
+
 for (var z=0; z<inputParsed.length; z++) {
 	inputParsed[z] = inputParsed[z].split(' '); 
 }
 console.log(inputParsed);
-for (var i=0; i<inputParsed.length; i++) { // create firewall, populate rows in input
-// do something with the data?
-}
-
 
 answer1 = 0;
 
-console.log('Answer to Day 18 Part 1 = ' + answer1);
+console.log('Answer to Day 21 Part 1 = ' + answer1);
 // Now for Part 2
-console.log('Answer to Day 18 Part 2 = ' + answer2);
+console.log('Answer to Day 21 Part 2 = ' + answer2);
