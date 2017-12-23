45d74dbfe5d731d816fa896dc744f108bb535d8c
gerty <gerty33@gmail.com>
2017-12-22 19:08:11 -0600

    Checkpoint before I realized that arrays are passed by reference.

--- Day 21 - Fractal Art/AoC2017_Day_21.js
+++ Day 21 - Fractal Art/AoC2017_Day_21.js
@@ -18,18 +18,7 @@ ruleInput = [];  // a list of strings defining inputs to enhancement
 ruleOutput = [];  // a list of strings defining outputs of enhancement
 myBigSquare = [];
 
-// How long is the input? A useful thing to know.
-console.log('Today\'s input has ' + dailyInput.length + ' characters.');
-
-var inputParsed = dailyInput.split('\n'); // splitting the input into lines by newline
-
-for (var z=0; z<inputParsed.length; z++) {
-	inputParsed[z] = inputParsed[z].split(' ');
-	//console.log(inputParsed[z]);
-	ruleInput[z] = inputParsed[z][0].split('/').join(); // take out the '/' from string
-	ruleOutput[z] = inputParsed[z][2].split('/').join();	
-}           // now we have our input mapping rules
-
+//////////////////////////////////////////////////
 function rotateCW(inSquare, times) { // rotate a square 'times' number of times
 	var localArray = [];
 	var borderLen = Math.sqrt(inSquare.length);
@@ -47,27 +36,41 @@ function flipSquare(inSquare) {  // take an array and flip it over the X axis
 	for (var i=0; i<borderLen; i++) {
 		//cycle through and flip the thing
 	}
+	return inSquare;
 }
-
-// Turning this into a map, for ALL possibilities:
-var ruleMap = new Map();
-for (var m=0; m<ruleInput.length; m++) {
-	for (rot=0; rot<4; rot++) {  // rotate it 'rot' times and enter into solution map
-		ruleMap.set(rotateCW(ruleInput[m],rot),ruleOutput[m]); // non-flipped
-		ruleMap.set(flipSquare(rotateCW(ruleInput[m],rot)),ruleOutput[m]);  // flipped
-	}
-}
-//console.log(ruleMap.entries()); // this should show all 8 iterations of ruleInput
-
 
 function processSquare(inSquare) {
-	// take a string of 4 or 9 chars, and return the square based on my input
+	// take a string and return the square based on my input
 	// rules data are in ruleInput and ruleOutput (global arrays)
 	
 }
 
 function insertSquare(littleSquare, bigSquare, x, y) {
 	// takes a little array and puts it in to a big array at x,y
+}
+
+//////////////////////////////////////////////////
+// How long is the input? A useful thing to know.
+console.log('Today\'s input has ' + dailyInput.length + ' characters.');
+
+// dailyInput has one rule per line
+var inputParsed = dailyInput.split('\n'); // splitting the input into lines by newline
+
+// each line is formatted 'input => output' with a space between each of 3 sections
+for (var z=0; z<inputParsed.length; z++) {
+	inputParsed[z] = inputParsed[z].split(' ');
+	ruleInput[z] = inputParsed[z][0].split('/').join(''); // take out the '/' from string
+	ruleOutput[z] = inputParsed[z][2].split('/').join('');	
+}           // now we have our input mapping rules
+
+// Turning this into a map, for all 8 rotations and flips:
+var ruleMap = new Map();
+for (var m=0; m<ruleInput.length; m++) {
+	for (rot=0; rot<4; rot++) {  // rotate it 4 times and enter into solution map
+		ruleMap.set(rotateCW(ruleInput[m],rot),ruleOutput[m]); // non-flipped
+		ruleMap.set(flipSquare(rotateCW(ruleInput[m],rot)),ruleOutput[m]);  // flipped
+		console.log(rotateCW(ruleInput[m])); // this should show all 8 iterations of ruleInput
+	}
 }
 
 var iteration = 1;
