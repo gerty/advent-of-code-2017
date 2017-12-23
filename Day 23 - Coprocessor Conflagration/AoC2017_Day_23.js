257146e6fcdea19c1f141c6c26f944f96ee79513
gerty <gerty33@gmail.com>
2017-12-22 23:08:32 -0600

    Starting Day 23

---
+++ Day 23 - Coprocessor Conflagration/AoC2017_Day_23.js
@@ -0,0 +1,103 @@
+// Advent Of Code Puzzle #23 - Coprocessor Conflagration
+// Programming in JavaScript via command line node
+// Repo located at https://github.com/gerty/advent-of-code-2017
+// Comments and suggestions welcome
+
+// Happy Holidays! Related to day 18 - only did half of that one.. ugh!
+console.log('Hello to the world...day 23 is come!');
+
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+//var data = fs.readFileSync('test.txt');
+var dailyInput = data.toString();
+var answer1 = 0;
+var answer2 = 0;
+
+// How long is the input? A useful thing to know.
+console.log('Today\'s input has ' + dailyInput.length + ' characters.');
+
+var allInput = dailyInput.split('\n'); // splitting the input into lines by newline
+for (var z=0; z<allInput.length; z++) {
+	allInput[z] = allInput[z].split(' '); 
+}
+
+var frequency = 0;
+var myData = {};
+
+console.log(allInput);
+var i = 0;
+
+function getVal(str) { // find the variable value, or converts string to actual number
+	if (isNaN(parseInt(str))) {
+		return(myData[str]);
+	}
+	else {
+		return parseInt(str);
+	}
+}
+
+while ((i<allInput.length) && (i>=0)) { 
+	console.log(myData);
+	console.log('Current position in instructions: ' + i);
+	switch(allInput[i][0]) {
+		case 'snd' :
+			console.log('Playing sound with frequency: ' + getVal(allInput[i][1]));
+			soundLastPlayed = getVal(allInput[i][1]);
+			i++;
+			break;
+		case 'set' :
+			if (myData[allInput[i][1]] != undefined) {
+				myData[allInput[i][1]] = getVal(allInput[i][2]);
+				console.log('Adjusting old ' + allInput[i][1] + ' to ' + getVal(allInput[i][2]));
+			}
+			else {
+				myData[allInput[i][1]] = getVal(allInput[i][2]);
+				console.log('Setting new ' + allInput[i][1] + ' to ' + getVal(allInput[i][2]));
+			}
+			i++;
+			break;
+		case 'add' :
+			if (myData[allInput[i][1]] === undefined) myData[allInput[i][1]] = 0;
+			myData[allInput[i][1]] += getVal(allInput[i][2]);
+			console.log('Adding ' + getVal(allInput[i][2] + ' to ' + allInput[i][1]));
+			i++;
+			break;
+		case 'mul' :
+			if (myData[allInput[i][1]] === undefined) myData[allInput[i][1]] = 0;
+			myData[allInput[i][1]] *= getVal(allInput[i][2]);
+			console.log('Multiplying ' + getVal(allInput[i][2] + ' by ' + allInput[i][1]));
+			i++;
+			break;
+		case 'mod' :
+			if (myData[allInput[i][1]] === undefined) myData[allInput[i][1]] = 0;
+			myData[allInput[i][1]] %= getVal(allInput[i][2]);
+			console.log(allInput[i][1] + ' modulo ' + getVal(allInput[i][2]));
+			i++;
+			break;
+		case 'rcv' :
+			if (getVal(allInput[i][1]) != 0) {
+				myData[allInput[i][1]] = soundLastPlayed; // "recovering"?
+				console.log('Just recovered sound to ' + soundLastPlayed);
+				answer1 = soundLastPlayed;
+				i=-1;
+			}
+			i++;
+			break;
+		case 'jgz' : 
+			if (myData[allInput[i][1]] === 0) {
+				i++;
+			}
+			else {
+				console.log('Jump ahead ' + getVal(allInput[i][1]));
+				i = i + getVal(allInput[i][2]);
+			}
+			break;
+		default :
+			i++;
+			break;
+	}
+}
+
+console.log('Answer to Day 23 Part 1 = ' + answer1);
+// Now for Part 2
+console.log('Answer to Day 23 Part 2 = ' + answer2);
